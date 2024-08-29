df4f741eb9f9cb6e966f4bee81167fde74d0b36a
itsmesalim <itsme.salimullah@gmail.com>
2024-08-29 11:56:12 +0500

    Initial commit

---
+++ src/Components/winning-combnation.js
@@ -0,0 +1,54 @@
+// import React from 'react'
+
+// function WinningCombnation() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default WinningCombnation;
+
+export const WINNING_COMBINATIONS = [
+    [
+      { row: 0, column: 0 },
+      { row: 0, column: 1 },
+      { row: 0, column: 2 },
+    ],
+    [
+      { row: 1, column: 0 },
+      { row: 1, column: 1 },
+      { row: 1, column: 2 },
+    ],
+    [
+      { row: 2, column: 0 },
+      { row: 2, column: 1 },
+      { row: 2, column: 2 },
+    ],
+    [
+      { row: 0, column: 0 },
+      { row: 1, column: 0 },
+      { row: 2, column: 0 },
+    ],
+    [
+      { row: 0, column: 1 },
+      { row: 1, column: 1 },
+      { row: 2, column: 1 },
+    ],
+    [
+      { row: 0, column: 2 },
+      { row: 1, column: 2 },
+      { row: 2, column: 2 },
+    ],
+    [
+      { row: 0, column: 0 },
+      { row: 1, column: 1 },
+      { row: 2, column: 2 },
+    ],
+    [
+      { row: 0, column: 2 },
+      { row: 1, column: 1 },
+      { row: 2, column: 0 },
+    ],
+  ];
